48036c7032852c4d251069443a06e8d3452fb379
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2023-12-08 16:05:00 -0600

    Weather API Endpoint in JS

--- Assets/script.js
+++ Assets/script.js
@@ -1,16 +1,48 @@
-const lat = 37.7749; // replace with the latitude of the desired location
-const lon = -122.4194; // replace with the longitude of the desired location
-const apiKey = '6b85b5521cb7b577a6e51b36f03923b2'; // replace with your actual API key
+//interpolate variables into a string by using ` instead of '
+// const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
 //https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
-fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
-  .then(response => response.json())
+//https://www.w3schools.com/js/js_api_fetch.asp
+
+function getWeather(city, apiKey) {
+  return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+    .then(response => {
+      if(!response.ok) {
+        throw new Error('Network response was not ok.');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      return data;
+    })
+    .catch(error => {
+      console.log(error);
+      throw error;
+    })
+}
+
+const city = 'Austin'; 
+const apiKey = '6b85b5521cb7b577a6e51b36f03923b2';
+
+getWeather(city, apiKey)
   .then(data => {
-    // process the weather data here
     console.log(data);
   })
   .catch(error => {
-    // handle any errors
-    console.error(error);
+    console.log(error);
   });
+
+
+// fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+//   .then(response => response.json())
+//   .then(data => {
+//     // process the weather data here
+//     console.log(data);
+//   })
+//   .catch(error => {
+//     // handle any errors
+//     console.error(error);
+//   });
+
