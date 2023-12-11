6fe59cf4d4155c1c3a49eec2c563ea656e1747cd
aaron-ross-sanchez <147529595+aaron-ross-sanchez@users.noreply.github.com>
2023-12-11 11:33:57 -0600

    forecast API functionality

--- Assets/script.js
+++ Assets/script.js
@@ -27,9 +27,29 @@ function getWeather(city, apiKey) {
     .catch(error => {
       console.log(error);
       throw error;
-    })
-    
+    }) 
 }
+
+function getForecast(city, apiKey) {
+  return fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=10&appid=${apiKey}`)
+    .then(response => {
+      if(!response.ok) {
+        throw new Error('Network response was not ok.');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      return data;
+    })
+    .catch(error => {
+      console.log(error);
+      throw error;
+    }) 
+}
+
+//we want to get item 5 of the list array displayed, that should be noon of the next day
+//
 
 //this is the function to displayWeather
 //used a async function to use await keyword
@@ -40,8 +60,28 @@ async function displayWeather() {
   console.log(weatherData);
 
   if (weatherData) {
-    const { name, main, coord } = weatherData;
-    console.log(name);
+    const { name, dt, main, wind } = weatherData;
+    console.log("Name:", name);
+    console.log("Date:", dt);
+    console.log("Temperature:", main.temp);
+    console.log("Wind Speed:", wind.speed);
+    console.log("Humidity:", main.humidity);
+  }
+}
+
+async function displayForecast() {
+  //used await to make sure the getWeather function will finish before assigning its value
+  const forecastData = await getForecast(city, apiKey);
+  //will log the data from the getWeather function
+  console.log(forecastData);
+
+  if (forecastData) {
+    const { city, list } = forecastData;
+    console.log("Name:", city.name);
+    console.log("Date:", list[4].dt_txt);
+    console.log("Temperature:", list[4].main.temp);
+    console.log("Wind Speed:", list[4].wind.speed);
+    console.log("Humidity:", list[4].main.humidity);
   }
 }
 //this is a checklist of things I need to include in this api display call
@@ -51,7 +91,7 @@ async function displayWeather() {
 //Temperature
 //Wind
 //Humidity
-displayWeather();
+
 
 getWeather(city, apiKey)
   .then(data => {
@@ -61,25 +101,22 @@ getWeather(city, apiKey)
     console.log(error);
   });
 
+displayWeather();
+
+
+getForecast(city, apiKey)
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+displayForecast();
+
   //this is a checklist of things I need to include in the forecast api display call
   //Date
   //Current Icon of Weather
   //Temperature
   //Wind
   //Humidity
-
-
-
-
-
-// fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
-//   .then(response => response.json())
-//   .then(data => {
-//     // process the weather data here
-//     console.log(data);
-//   })
-//   .catch(error => {
-//     // handle any errors
-//     console.error(error);
-//   });
-
